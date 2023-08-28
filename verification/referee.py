92bbbfa54a420e504067f73b0d47e11d2c99c225
Oleksandr Zozulia <freemanlex@gmail.com>
2023-08-28 23:37:56 +0300

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -1,7 +1,7 @@
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io_template import CheckiOReferee
-# from checkio.referees.checkers import to_list
+from checkio.referees.checkers import to_list
 
 from tests import TESTS
 
